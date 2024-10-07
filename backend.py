aafd548f65f40c6a50b713d1668c926eb1a828c4
Aseem Mehrotra <67800004+aseemm84@users.noreply.github.com>
2024-10-07 15:59:17 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ groq = st.secrets["Groq_API_Key"]
 
 
 llm = ChatGroq(
-    model="llama-3.1-70b-versatile",
+    model="llama-3.1-8b-instant",
     groq_api_key=groq,
     temperature=0
     # other params...
