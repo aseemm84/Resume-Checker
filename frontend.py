99768d411e4785720fb2079a262a6b99e9b4e1cb
Aseem Mehrotra <67800004+aseemm84@users.noreply.github.com>
2024-10-07 18:00:03 +0530

    Add files via upload

--- frontend.py
+++ frontend.py
@@ -36,7 +36,7 @@ if st.button("Evaluate"):
                 with st.spinner("Evaluating your CV..."):
                     progress_bar = st.progress(0)
                     for i in range(100):
-                        time.sleep(2.5)  # Simulate processing time
+                        time.sleep(0.2)  # Simulate processing time
                         progress_bar.progress(i + 1)
                     result_struct = CVstruct_prompt(cv_content)
                     result_verb = actVerb_prompt(cv_content, job_description)
